8406d7c0a0efcbe3c9586a3d78901777c3310b12
Shourov00 <sfazleyrabbi@gmail.com>
2024-03-17 02:34:54 +0600

    assets updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,7 +70,7 @@ module.exports = {
       },
       backgroundImage: {
         "purple-gradient": "url('/assets/images/gradient-bg.svg')",
-        banner: "url('/assets/images/banner-bg.png')",
+        banner: "url('/assets/images/banner-sky.png')",
       },
       borderRadius: {
         lg: "var(--radius)",
